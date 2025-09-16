dbf9e6e29e278e254c22b8cf6ceb6ee6caaa6921
natuan05 <160258271+natuan05@users.noreply.github.com>
2025-09-16 17:23:50 +0700

    top bar

--- app/src/main/java/com/example/buildagrid/MainActivity.kt
+++ app/src/main/java/com/example/buildagrid/MainActivity.kt
@@ -5,19 +5,23 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.buildagrid.data.DataSource
 import com.example.buildagrid.ui.CourseGrid
 import com.example.buildagrid.ui.theme.BuildAGridTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.unit.dp
 
 @OptIn(ExperimentalMaterial3Api::class)
 class MainActivity : ComponentActivity() {
@@ -28,14 +32,18 @@ class MainActivity : ComponentActivity() {
             BuildAGridTheme {
                 Scaffold(
                     topBar = {
-                        TopAppBar(
-                            title = { Text("Courses") }
+                        CenterAlignedTopAppBar(
+                            title = {},
+                            modifier = Modifier.height(56.dp),
+                            colors = TopAppBarDefaults.centerAlignedTopAppBarColors(
+                                containerColor = MaterialTheme.colorScheme.primary
+                            )
                         )
                     }
                 ) { innerPadding ->
                     CourseGrid(
                         topics = DataSource.topics,
-                        modifier = Modifier.padding(innerPadding) // ✅ tránh đè status bar
+                        modifier = Modifier.padding(innerPadding)
                     )
                 }
             }

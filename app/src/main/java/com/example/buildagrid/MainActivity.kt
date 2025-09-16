699584dce84883fb9a5c2328bec3061fd92663b9
natuan05 <160258271+natuan05@users.noreply.github.com>
2025-09-16 15:39:26 +0700

    topappbar

--- app/src/main/java/com/example/buildagrid/MainActivity.kt
+++ app/src/main/java/com/example/buildagrid/MainActivity.kt
@@ -6,23 +6,36 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material3.TopAppBar
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.buildagrid.data.DataSource
+import com.example.buildagrid.ui.CourseGrid
 import com.example.buildagrid.ui.theme.BuildAGridTheme
+import androidx.compose.runtime.Composable
 
+@OptIn(ExperimentalMaterial3Api::class)
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             BuildAGridTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
+                Scaffold(
+                    topBar = {
+                        TopAppBar(
+                            title = { Text("Courses") }
+                        )
+                    }
+                ) { innerPadding ->
+                    CourseGrid(
+                        topics = DataSource.topics,
+                        modifier = Modifier.padding(innerPadding) // ✅ tránh đè status bar
                     )
                 }
             }
@@ -30,18 +43,10 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+@Preview
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
+fun CourseGridPreview() {
     BuildAGridTheme {
-        Greeting("Android")
+        CourseGrid(topics = DataSource.topics)
     }
 }
